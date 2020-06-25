859b1c455628703f56979f004c251ead2a35027c
Mina G <mghaffar89@gmail.com>
2020-06-25 15:57:23 -0500

    added the npm install file and fixed server

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,25 @@
-var http = require("http");
-var PORT = 8080;
-var server = http.createServer(handleRequest);
+//Set up Dependencies
+var path = require("path");
+var express = require("express");
 
+//set up port
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//HTML GET Routes
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
+//listener will start the server
 server.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
 });
