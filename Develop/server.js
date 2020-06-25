4b6c409a8a9d02ebfd3d39a98ae3ba8efd1def53
Mina G <mghaffar89@gmail.com>
2020-06-25 16:10:35 -0500

    created the API routes with get, post, delete

--- Develop/server.js
+++ Develop/server.js
@@ -21,6 +21,25 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
+//API Routes
+//GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", function (req, res) {
+  res.json(db.json);
+});
+
+//POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+app.post("/api/notes", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newNote = req.body;
+
+  console.log(newNote);
+
+  characters.push(newNote);
+
+  res.json(newNote);
+});
+
 //listener will start the server
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
