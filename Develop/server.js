80d363ca03657e4abe1d323b4f5b39d32ff335a4
Mina G <mghaffar89@gmail.com>
2020-06-29 23:05:21 -0500

    added delete function with ID parameters

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,7 @@
 const path = require("path");
 const express = require("express");
 const fs = require("fs");
+const util = require("util");
 
 //set up port
 const PORT = process.env.PORT || 8080;
@@ -15,49 +16,59 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "./public")));
 
+//Set up the read and write files
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 //HTML GET Routes
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
 //API Routes
 //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", function (req, res) {
-  res.sendFile(path.join(_dirname, './db/db.json'))
+  res.sendFile(path.join(_dirname, "./db/db.json"));
 });
 
 //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  const addedNote = JSON.stringify(req.body);
-  fs.readFile('./db/db.json', 'utf8', (err,data) => {
-if (err) throw err; 
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+  };
 
-let dataArray = JSON.parse(data);
-let lastNoteId = dataArray[dataArray.length - 1].id; 
+  const notes = JSON.parse(fs.readFileSync(_dirname + "/db/db.json"));
+  newNote.id = notes.length;
+  notes.push(newNote);
 
-if (lastNoteId === undefined) {
-  lastNoteId = 0; 
-}
+  fs.writeFileSync(__dirname + "./db/db.json", JSON.stringify(notes));
 
-let newId = lastNoteId - 1; 
+  res.json(notes);
+});
 
-addedNote = 
-  }
+app.delete("/api/notes/:id", function (req, res) {
+  const noteId = req.params.id;
 
-  console.log(newNote);
+  let notes = JSON.parse(fs.readFileSync(__dirname + "/db/db.json"));
+  notes = notes.filter(function (note, index) {
+    if (noteId == index) {
+      return false;
+    }
 
-  characters.push(newNote);
+    return true;
+  });
 
-  res.json(newNote);
+  fs.writeFileSync(__dirname + "/db/db.json", JSON.stringify(notes));
+
+  res.json(notes);
 });
 
 //listener will start the server
