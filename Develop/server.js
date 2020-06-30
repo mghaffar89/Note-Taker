cd95ce9d0d8a5db5fe6392d99dd7823d7ea65bee
Mina G <mghaffar89@gmail.com>
2020-06-29 22:50:33 -0500

    adding function for new note

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@
 //Set up Dependencies
 const path = require("path");
 const express = require("express");
-const fs = require(fs);
+const fs = require("fs");
 
 //set up port
 const PORT = process.env.PORT || 8080;
@@ -12,6 +12,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 //HTML GET Routes
 app.get("/notes", function (req, res) {
@@ -25,14 +30,28 @@ app.get("*", function (req, res) {
 //API Routes
 //GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", function (req, res) {
-  res.json(db.json);
+  res.sendFile(path.join(_dirname, './db/db.json'))
 });
 
 //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes", function (req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  const newNote = req.body;
+  const addedNote = JSON.stringify(req.body);
+  fs.readFile('./db/db.json', 'utf8', (err,data) => {
+if (err) throw err; 
+
+let dataArray = JSON.parse(data);
+let lastNoteId = dataArray[dataArray.length - 1].id; 
+
+if (lastNoteId === undefined) {
+  lastNoteId = 0; 
+}
+
+let newId = lastNoteId - 1; 
+
+addedNote = 
+  }
 
   console.log(newNote);
 
